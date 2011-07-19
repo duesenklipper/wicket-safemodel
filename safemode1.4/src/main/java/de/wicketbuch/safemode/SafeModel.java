dffc9e910009004f9d82799a242fef43d00f2c17
Carl-Eric Menzel <cmenzel@wicketbuch.de>
2011-07-19 13:54:06 +0200

    corrected error message

--- safemode1.4/src/main/java/de/wicketbuch/safemode/SafeModel.java
+++ safemode1.4/src/main/java/de/wicketbuch/safemode/SafeModel.java
@@ -10,91 +10,85 @@ import org.jmock.api.Invokable;
 import org.jmock.lib.legacy.ClassImposteriser;
 
 public final class SafeModel {
-	private SafeModel() {
-		// prevent instantiation
-	}
+    private SafeModel() {
+        // prevent instantiation
+    }
 
-	public static interface PropertyFinder {
-		// marker
-	}
+    public static interface PropertyFinder {
+        // marker
+    }
 
-	private final static ThreadLocal<StringBuilder> path = new ThreadLocal<StringBuilder>();
-	private final static ThreadLocal<Object> root = new ThreadLocal<Object>();
-	private static final ThreadLocal<Object> currentTarget = new ThreadLocal<Object>();
+    private final static ThreadLocal<StringBuilder> path = new ThreadLocal<StringBuilder>();
+    private final static ThreadLocal<Object> root = new ThreadLocal<Object>();
+    private static final ThreadLocal<Object> currentTarget = new ThreadLocal<Object>();
 
-	private static class PropertyFinderImpl implements PropertyFinder,
-			Invokable {
-		private static final PropertyFinderImpl INSTANCE = new PropertyFinderImpl();
+    private static class PropertyFinderImpl implements PropertyFinder, Invokable {
+        private static final PropertyFinderImpl INSTANCE = new PropertyFinderImpl();
 
-		private PropertyFinderImpl() {
-		}
+        private PropertyFinderImpl() {
+        }
 
-		public Object invoke(final Invocation invocation) throws Throwable {
-			final Method method = invocation.getInvokedMethod();
-			final String methodName = method.getName();
-			final StringBuilder pathBuilder = path.get();
-			final Object current = currentTarget.get();
-			final Object callResult = current != null ? method.invoke(current,
-					invocation.getParametersAsArray()) : null;
-			final Class<?> returnType = callResult != null ? callResult
-					.getClass() : method.getReturnType();
-			if (methodName.equals("get")
-					&& (invocation.getParameterCount() == 1)) {
-				pathBuilder.append("[");
-				pathBuilder.append(invocation.getParameter(0));
-				pathBuilder.append("]");
-			} else if (methodName.startsWith("get")) {
-				final String propertyName = new StringBuilder()
-						.append(Character.toLowerCase(methodName.charAt(3)))
-						.append(methodName.substring(4)).toString();
-				if (pathBuilder.length() > 0) {
-					pathBuilder.append(".");
-				}
-				pathBuilder.append(propertyName);
-			} else {
-				throw new UnsupportedOperationException(
-						"SafeModel only supports getters");
-			}
-			currentTarget.set(callResult);
-			if (returnType.isPrimitive()) {
-				return callResult;
-			}
-			if (Modifier.isFinal(returnType.getModifiers())) {
-				return callResult;
-			} else if (Object.class.equals(returnType)) {
-				return ClassImposteriser.INSTANCE.imposterise(INSTANCE,
-						PropertyFinder.class);
-			} else {
-				return ClassImposteriser.INSTANCE.imposterise(INSTANCE,
-						returnType, PropertyFinder.class);
-			}
-		}
-	}
+        public Object invoke(final Invocation invocation) throws Throwable {
+            final Method method = invocation.getInvokedMethod();
+            final String methodName = method.getName();
+            final StringBuilder pathBuilder = path.get();
+            final Object current = currentTarget.get();
+            final Object callResult = current != null ? method.invoke(current,
+                    invocation.getParametersAsArray()) : null;
+            final Class<?> returnType = callResult != null ? callResult.getClass() : method
+                    .getReturnType();
+            if (methodName.equals("get") && (invocation.getParameterCount() == 1)) {
+                pathBuilder.append("[");
+                pathBuilder.append(invocation.getParameter(0));
+                pathBuilder.append("]");
+            } else if (methodName.startsWith("get")) {
+                final String propertyName = new StringBuilder()
+                        .append(Character.toLowerCase(methodName.charAt(3)))
+                        .append(methodName.substring(4)).toString();
+                if (pathBuilder.length() > 0) {
+                    pathBuilder.append(".");
+                }
+                pathBuilder.append(propertyName);
+            } else {
+                throw new UnsupportedOperationException(
+                        "SafeModel only supports JavaBean-style getters");
+            }
+            currentTarget.set(callResult);
+            if (returnType.isPrimitive()) {
+                return callResult;
+            }
+            if (Modifier.isFinal(returnType.getModifiers())) {
+                return callResult;
+            } else if (Object.class.equals(returnType)) {
+                return ClassImposteriser.INSTANCE.imposterise(INSTANCE, PropertyFinder.class);
+            } else {
+                return ClassImposteriser.INSTANCE.imposterise(INSTANCE, returnType,
+                        PropertyFinder.class);
+            }
+        }
+    }
 
-	public static <T> IModel<T> model(final T metaTarget) {
-		final Object target = root.get();
-		root.remove();
-		final StringBuilder pathBuilder = path.get();
-		path.remove();
-		currentTarget.remove();
-		if (target == null) {
-			throw new IllegalArgumentException(
-					"target not set - did you forget to use from()?");
-		}
-		if (pathBuilder == null) {
-			throw new IllegalArgumentException(
-					"target not set - did you forget to use from()?");
-		}
-		return new PropertyModel<T>(target, pathBuilder.toString());
-	}
+    public static <T> IModel<T> model(final T metaTarget) {
+        final Object target = root.get();
+        root.remove();
+        final StringBuilder pathBuilder = path.get();
+        path.remove();
+        currentTarget.remove();
+        if (target == null) {
+            throw new IllegalArgumentException("target not set - did you forget to use from()?");
+        }
+        if (pathBuilder == null) {
+            throw new IllegalArgumentException("path not set - did you forget to use from()?");
+        }
+        return new PropertyModel<T>(target, pathBuilder.toString());
+    }
 
-	@SuppressWarnings("unchecked")
-	public static <U> U from(final U target) {
-		path.set(new StringBuilder());
-		root.set(target);
-		currentTarget.set(target);
-		return (U) ClassImposteriser.INSTANCE.imposterise(
-				PropertyFinderImpl.INSTANCE, target.getClass(),
-				PropertyFinder.class);
-	}
+    @SuppressWarnings("unchecked")
+    public static <U> U from(final U target) {
+        path.set(new StringBuilder());
+        root.set(target);
+        currentTarget.set(target);
+        return (U) ClassImposteriser.INSTANCE.imposterise(PropertyFinderImpl.INSTANCE,
+                target.getClass(), PropertyFinder.class);
+    }
 }
--- safemode1.4/src/test/java/de/wicketbuch/safemode/SafeModelTest.java
+++ safemode1.4/src/test/java/de/wicketbuch/safemode/SafeModelTest.java
@@ -1,11 +1,7 @@
 package de.wicketbuch.safemode;
 
-import static de.wicketbuch.safemode.SafeModel.from;
-import static de.wicketbuch.safemode.SafeModel.model;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertSame;
+import static de.wicketbuch.safemode.SafeModel.*;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,182 +16,181 @@ import org.junit.Test;
 
 public class SafeModelTest {
 
-	private WicketTester tester;
+    private WicketTester tester;
 
-	@Before
-	public void setUp() {
-		this.tester = new WicketTester();
-	}
+    @Before
+    public void setUp() {
+        this.tester = new WicketTester();
+    }
 
-	@After
-	public void tearDown() {
-		this.tester.destroy();
-	}
+    @After
+    public void tearDown() {
+        this.tester.destroy();
+    }
 
-	public static class Top {
-		private Middle mid;
-		private List<Middle> mids = new ArrayList<Middle>();
-		private Map<String, Bottom> bottomMap = new HashMap<String, Bottom>();
+    public static class Top {
+        private Middle mid;
+        private List<Middle> mids = new ArrayList<Middle>();
+        private Map<String, Bottom> bottomMap = new HashMap<String, Bottom>();
 
-		public Map<String, Bottom> getBottomMap() {
-			return bottomMap;
-		}
+        public Map<String, Bottom> getBottomMap() {
+            return bottomMap;
+        }
 
-		public void setBottomMap(final Map<String, Bottom> bottomMap) {
-			this.bottomMap = bottomMap;
-		}
+        public void setBottomMap(final Map<String, Bottom> bottomMap) {
+            this.bottomMap = bottomMap;
+        }
 
-		public Middle getMid() {
-			return mid;
-		}
+        public Middle getMid() {
+            return mid;
+        }
 
-		public void setMid(final Middle mid) {
-			this.mid = mid;
-		}
+        public void setMid(final Middle mid) {
+            this.mid = mid;
+        }
 
-		public List<Middle> getMids() {
-			return mids;
-		}
+        public List<Middle> getMids() {
+            return mids;
+        }
 
-		public void setMids(final List<Middle> mids) {
-			this.mids = mids;
-		}
-	}
+        public void setMids(final List<Middle> mids) {
+            this.mids = mids;
+        }
+    }
 
-	public static class Middle {
-		private String string;
-		private Bottom bot;
+    public static class Middle {
+        private String string;
+        private Bottom bot;
 
-		public String getString() {
-			return string;
-		}
+        public String getString() {
+            return string;
+        }
 
-		public void setString(final String string) {
-			this.string = string;
-		}
+        public void setString(final String string) {
+            this.string = string;
+        }
 
-		public Bottom getBot() {
-			return bot;
-		}
+        public Bottom getBot() {
+            return bot;
+        }
 
-		public void setBot(final Bottom bot) {
-			this.bot = bot;
-		}
-	}
+        public void setBot(final Bottom bot) {
+            this.bot = bot;
+        }
+    }
 
-	public static class Bottom {
-		private int value = 0;
+    public static class Bottom {
+        private int value = 0;
 
-		public int getValue() {
-			return value;
-		}
+        public int getValue() {
+            return value;
+        }
 
-		public void setValue(final int value) {
-			this.value = value;
-		}
-	}
+        public void setValue(final int value) {
+            this.value = value;
+        }
+    }
 
-	@Test
-	public void simplePropertyModel() throws Exception {
-		final Middle mid = new Middle();
-		mid.setString("testString");
-		final IModel<String> model = model(from(mid).getString());
-		assertEquals("testString", model.getObject());
-		model.setObject("newString");
-		assertEquals("newString", mid.getString());
-	}
+    @Test
+    public void simplePropertyModel() throws Exception {
+        final Middle mid = new Middle();
+        mid.setString("testString");
+        final IModel<String> model = model(from(mid).getString());
+        assertEquals("testString", model.getObject());
+        model.setObject("newString");
+        assertEquals("newString", mid.getString());
+    }
 
-	@Test
-	public void multiStepPropertyModel() throws Exception {
-		final Top top = new Top();
-		top.setMid(new Middle());
-		top.getMid().setString("testString");
-		final IModel<String> model = model(from(top).getMid().getString());
-		assertEquals("testString", model.getObject());
-	}
+    @Test
+    public void multiStepPropertyModel() throws Exception {
+        final Top top = new Top();
+        top.setMid(new Middle());
+        top.getMid().setString("testString");
+        final IModel<String> model = model(from(top).getMid().getString());
+        assertEquals("testString", model.getObject());
+    }
 
-	@Test
-	public void customBeanPropertyModel() throws Exception {
-		final Top top = new Top();
-		top.setMid(new Middle());
-		final Bottom bot = new Bottom();
-		bot.setValue(42);
-		top.getMid().setBot(bot);
-		final IModel<Bottom> model = model(from(top).getMid().getBot());
-		assertSame(bot, model.getObject());
-	}
+    @Test
+    public void customBeanPropertyModel() throws Exception {
+        final Top top = new Top();
+        top.setMid(new Middle());
+        final Bottom bot = new Bottom();
+        bot.setValue(42);
+        top.getMid().setBot(bot);
+        final IModel<Bottom> model = model(from(top).getMid().getBot());
+        assertSame(bot, model.getObject());
+    }
 
-	@Test
-	public void nullLeafModel() throws Exception {
-		final Top top = new Top();
-		final IModel<Middle> model = model(from(top).getMid());
-		assertNull(model.getObject());
-		final Middle mid = new Middle();
-		model.setObject(mid);
-		assertSame(mid, top.getMid());
-	}
+    @Test
+    public void nullLeafModel() throws Exception {
+        final Top top = new Top();
+        final IModel<Middle> model = model(from(top).getMid());
+        assertNull(model.getObject());
+        final Middle mid = new Middle();
+        model.setObject(mid);
+        assertSame(mid, top.getMid());
+    }
 
-	@Test
-	public void nullInTreeModel() throws Exception {
-		final Top top = new Top();
-		final IModel<Bottom> model = model(from(top).getMid().getBot());
-		assertNull(model.getObject());
-	}
+    @Test
+    public void nullInTreeModel() throws Exception {
+        final Top top = new Top();
+        final IModel<Bottom> model = model(from(top).getMid().getBot());
+        assertNull(model.getObject());
+    }
 
-	@Test
-	public void listAsLeaf() throws Exception {
-		final Top top = new Top();
-		final Middle mid = new Middle();
-		top.getMids().add(mid);
-		final IModel<Middle> model = model(from(top).getMids().get(0));
-		assertSame(mid, model.getObject());
-		final Middle newMid = new Middle();
-		model.setObject(newMid);
-		assertNotSame(mid, model.getObject());
-		assertSame(newMid, model.getObject());
-		assertSame(newMid, top.getMids().get(0));
-		assertEquals(1, top.getMids().size());
-	}
+    @Test
+    public void listAsLeaf() throws Exception {
+        final Top top = new Top();
+        final Middle mid = new Middle();
+        top.getMids().add(mid);
+        final IModel<Middle> model = model(from(top).getMids().get(0));
+        assertSame(mid, model.getObject());
+        final Middle newMid = new Middle();
+        model.setObject(newMid);
+        assertNotSame(mid, model.getObject());
+        assertSame(newMid, model.getObject());
+        assertSame(newMid, top.getMids().get(0));
+        assertEquals(1, top.getMids().size());
+    }
 
-	@Test
-	public void listInPath() throws Exception {
-		final Top top = new Top();
-		final Middle mid = new Middle();
-		top.getMids().add(mid);
-		final IModel<Bottom> model = model(from(top).getMids().get(0).getBot());
-		assertNull(model.getObject());
-		final Bottom bot = new Bottom();
-		model.setObject(bot);
-		assertSame(bot, model.getObject());
-		assertSame(bot, top.getMids().get(0).getBot());
-	}
+    @Test
+    public void listInPath() throws Exception {
+        final Top top = new Top();
+        final Middle mid = new Middle();
+        top.getMids().add(mid);
+        final IModel<Bottom> model = model(from(top).getMids().get(0).getBot());
+        assertNull(model.getObject());
+        final Bottom bot = new Bottom();
+        model.setObject(bot);
+        assertSame(bot, model.getObject());
+        assertSame(bot, top.getMids().get(0).getBot());
+    }
 
-	@Test
-	public void mapAsLeaf() throws Exception {
-		final Top top = new Top();
-		final Bottom bot1 = new Bottom();
-		top.getBottomMap().put("bot1", bot1);
-		final Bottom bot2 = new Bottom();
-		top.getBottomMap().put("bot2", bot2);
-		final IModel<Bottom> model = model(from(top).getBottomMap().get("bot1"));
-		assertEquals(bot1, model.getObject());
-		model.setObject(bot2);
-		assertEquals(bot2, top.getBottomMap().get("bot1")); // should point to
-															// bot2 now
-	}
+    @Test
+    public void mapAsLeaf() throws Exception {
+        final Top top = new Top();
+        final Bottom bot1 = new Bottom();
+        top.getBottomMap().put("bot1", bot1);
+        final Bottom bot2 = new Bottom();
+        top.getBottomMap().put("bot2", bot2);
+        final IModel<Bottom> model = model(from(top).getBottomMap().get("bot1"));
+        assertEquals(bot1, model.getObject());
+        model.setObject(bot2);
+        assertEquals(bot2, top.getBottomMap().get("bot1")); // should point to
+                                                            // bot2 now
+    }
 
-	@Test
-	public void mapInPath() throws Exception {
-		final Top top = new Top();
-		final Bottom bot1 = new Bottom();
-		top.getBottomMap().put("bot1", bot1);
-		bot1.setValue(42);
-		final Bottom bot2 = new Bottom();
-		top.getBottomMap().put("bot2", bot2);
-		final IModel<Integer> model = model(from(top).getBottomMap()
-				.get("bot1").getValue());
-		assertEquals(Integer.valueOf(42), model.getObject());
-		model.setObject(43);
-		assertEquals(43, bot1.getValue());
-	}
+    @Test
+    public void mapInPath() throws Exception {
+        final Top top = new Top();
+        final Bottom bot1 = new Bottom();
+        top.getBottomMap().put("bot1", bot1);
+        bot1.setValue(42);
+        final Bottom bot2 = new Bottom();
+        top.getBottomMap().put("bot2", bot2);
+        final IModel<Integer> model = model(from(top).getBottomMap().get("bot1").getValue());
+        assertEquals(Integer.valueOf(42), model.getObject());
+        model.setObject(43);
+        assertEquals(43, bot1.getValue());
+    }
 }
